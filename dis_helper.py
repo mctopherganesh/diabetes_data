1660e0ee6b6b4cfb34eb3806851975a3139b25f6
Christopher Ganesh McMahon <mctopherganesh@gmail.com>
2020-06-07 14:35:10 -0500

    added  function that updates a food database

--- dis_helper.py
+++ dis_helper.py
@@ -6,14 +6,23 @@ def db_and_table_name_setter(name_of_db, name_of_table):
     return name_of_db, name_of_table
 
 
-def sqlite_create_table(name_of_db, name_of_table):
+def create_table_for_bs(name_of_db, name_of_table):
+    # function for testing and creating bs data table
     conn = sqlite3.connect(name_of_db) # previously 'bs.db'
     c = conn.cursor()
     c.execute('create table if not exists {}(date_measure_taken text, bs_measure real)'.format(name_of_table))
     c.close()
     conn.close()
+
+def create_table_for_food(name_of_db, name_of_table):
+    # function for testing and creating bs data table
+    conn = sqlite3.connect(name_of_db) # previously 'bs.db'
+    c = conn.cursor()
+    c.execute('create table if not exists {}(date_measure_taken text, food_consumed text)'.format(name_of_table))
+    c.close()
+    conn.close()
     
-def sqlite_data_entry(new_data_row, name_of_db, name_of_table):
+def data_entry(new_data_row, name_of_db, name_of_table):
     conn = sqlite3.connect(name_of_db)
     c = conn.cursor()
     c.execute('insert into {} values{}'.format(name_of_table, new_data_row))
@@ -26,7 +35,7 @@ def return_date():
     return x.strftime("%Y-%m-%d %H:%M:%S")
 
 
-def reads_in_bs(bs):
+def reads_in_time_and_data(data):
     dtstmp = return_date()
-    return "('{}',{})".format(dtstmp, bs)
+    return "('{}',{})".format(dtstmp, data)
 
--- dis_test.py
+++ dis_test.py
@@ -17,17 +17,26 @@ async def ping(ctx):
 async def bs(ctx, arg):
     db_name, table_name = dis_helper.db_and_table_name_setter('test.db','blood_sugar')
     bs = arg.strip()
-    dis_helper.sqlite_create_table(db_name, table_name)
-    dtstmp = dis_helper.return_date()
-    new_row = dis_helper.reads_in_bs(bs)
-    dis_helper.sqlite_data_entry(new_row,db_name,table_name)
+    # TODO: try/asser that bs is an integer
+    dis_helper.create_table_for_bs(db_name, table_name)
+    # dtstmp = dis_helper.return_date()
+    new_row = dis_helper.reads_in_time_and_data(bs)
+    dis_helper.data_entry(new_row,db_name,table_name)
     await ctx.send('new row written!')
 
 
 @client.command()
 async def food(ctx, *args):
     
-    await ctx.send('new row written!')
+    food_string = ', '.join(args)
+    db_name, table_name = dis_helper.db_and_table_name_setter('test_food.db','food')
+    dis_helper.create_table_for_food(db_name, table_name)
+    new_row = dis_helper.reads_in_time_and_data(food_string)
+    dis_helper.data_entry(new_row, db_name, table_name)
+
+
+
+    await ctx.send('you ate ' + food_string)
     
     
     
