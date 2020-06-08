31aabc001e0d8db211b31943f2b770b2ab5b326e
Christopher Ganesh McMahon <mctopherganesh@gmail.com>
2020-06-07 23:08:28 -0500

    tries to deploy to heroku

--- dis_helper.py
+++ dis_helper.py
@@ -18,14 +18,14 @@ def create_table_for_food(name_of_db, name_of_table):
     # function for testing and creating bs data table
     conn = sqlite3.connect(name_of_db) # previously 'bs.db'
     c = conn.cursor()
-    c.execute('create table if not exists {}(date_measure_taken text, food_consumed text)'.format(name_of_table))
+    c.execute('create table if not exists {}(date_measure_taken text, consumed text)'.format(name_of_table))
     c.close()
     conn.close()
     
 def data_entry(new_data_row, name_of_db, name_of_table):
     conn = sqlite3.connect(name_of_db)
     c = conn.cursor()
-    c.execute('insert into {} values{}'.format(name_of_table, new_data_row))
+    c.execute('insert into {} values {}'.format(name_of_table, new_data_row))
     conn.commit() # have to run this after changing the table
     c.close()  # closes the connection to the cursor
     conn.close()
@@ -38,4 +38,8 @@ def return_date():
 def reads_in_time_and_data(data):
     dtstmp = return_date()
     return "('{}',{})".format(dtstmp, data)
+
+def reads_in_time_and_data_for_food(data):
+    dtstmp = return_date()
+    return "('{}','{}')".format(dtstmp, data)
 
--- dis_test.py
+++ dis_test.py
@@ -17,26 +17,37 @@ async def ping(ctx):
 async def bs(ctx, arg):
     db_name, table_name = dis_helper.db_and_table_name_setter('test.db','blood_sugar')
     bs = arg.strip()
+    print(args)
+    print(type(bs))
     # TODO: try/asser that bs is an integer
     dis_helper.create_table_for_bs(db_name, table_name)
     # dtstmp = dis_helper.return_date()
     new_row = dis_helper.reads_in_time_and_data(bs)
     dis_helper.data_entry(new_row,db_name,table_name)
-    await ctx.send('new row written!')
+    
+    await ctx.send('new row written in ' + str(table_name) + ' table.')
 
 
 @client.command()
-async def food(ctx, *args):
+async def food(ctx, *, arg):
+    db_name, table_name = dis_helper.db_and_table_name_setter('test_0.db','foods')
     
-    food_string = ', '.join(args)
-    db_name, table_name = dis_helper.db_and_table_name_setter('test_food.db','food')
+
     dis_helper.create_table_for_food(db_name, table_name)
-    new_row = dis_helper.reads_in_time_and_data(food_string)
+
+    new_row = dis_helper.reads_in_time_and_data_for_food(arg)
+
     dis_helper.data_entry(new_row, db_name, table_name)
 
+    await ctx.send('new row written in the ' + str(table_name) + ' table.')
 
 
-    await ctx.send('you ate ' + food_string)
+@client.command()
+async def pic(ctx):
+    await ctx.send(file=discord.File('bar_test_png.png'))
+
+
+
     
     
     
